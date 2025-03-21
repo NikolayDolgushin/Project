e8a37b05a05c9290d87510946416684374a0d89f
NikolayDolgushin <nikolaydnv@inbox.ru>
2025-03-21 21:19:31 +0500

    1

--- clothesapp/models.py
+++ clothesapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    name = models.CharField(
+        verbose_name="COLOR",
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Metha:
+        verbose_name = "COLOR"
+        verbose_name_plural = "COLORS"
+
+
+class Clothes(models.Model):
+
+    name = models.CharField(
+        verbose_name="ITEM",
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    color = models.ForeignKey(Color, verbose_name="COLOR", on_delete=models.CASCADE)
+    price = models.IntegerField('PRICE', null=False, blank=False)
+    pic = models.ImageField('PICTURE', upload_to='pics/')
+    backpic = models.ImageField('BACK PICTURE', upload_to='backpics/')
+
+    def __str__(self):
+        return f'{self.color.name} {self.name}'
+
+    def save(self, *args, **kwargs):
+        if self.price < 0:
+            pass
+        super().save()
+
+    class Meta:
+        verbose_name = "CLOTHING"
+        verbose_name_plural = "CLOTHES"
---
+++ clothesapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.Main.as_view(), name='main'),
+    path('clothes/<int:pk>/', views.ClothesView.as_view(), name='clothes_view'),
+    ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
